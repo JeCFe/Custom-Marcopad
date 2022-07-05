d245945c0344e60d7e5a0d34b5cf423733564a06
JeCFe <38367384+JeCFe@users.noreply.github.com>
2022-07-06 00:29:52 +0100

    Updates

---
+++ 3. Customisation/Macropad Customisation GUI Forms/Form1.Designer.cs
@@ -0,0 +1,405 @@
+﻿namespace Macropad_Customisation_GUI_Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnNine = new System.Windows.Forms.Button();
+            this.btnEight = new System.Windows.Forms.Button();
+            this.btnSeven = new System.Windows.Forms.Button();
+            this.btnSix = new System.Windows.Forms.Button();
+            this.btnFive = new System.Windows.Forms.Button();
+            this.btnFour = new System.Windows.Forms.Button();
+            this.btnThree = new System.Windows.Forms.Button();
+            this.btnTwo = new System.Windows.Forms.Button();
+            this.btnOne = new System.Windows.Forms.Button();
+            this.txtVisPadDisplay = new System.Windows.Forms.TextBox();
+            this.txtButtonName = new System.Windows.Forms.TextBox();
+            this.lblNewButtonName = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnNewLayer = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.txtSetLayerName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSetLayername = new System.Windows.Forms.Button();
+            this.lblSelectedButton = new System.Windows.Forms.Label();
+            this.lblSelectedButtonName = new System.Windows.Forms.Label();
+            this.lblSelectedButtonOperation = new System.Windows.Forms.Label();
+            this.lblNewButtonCommand = new System.Windows.Forms.Label();
+            this.txtNewButtonCommand = new System.Windows.Forms.TextBox();
+            this.btnSetButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnNine);
+            this.groupBox1.Controls.Add(this.btnEight);
+            this.groupBox1.Controls.Add(this.btnSeven);
+            this.groupBox1.Controls.Add(this.btnSix);
+            this.groupBox1.Controls.Add(this.btnFive);
+            this.groupBox1.Controls.Add(this.btnFour);
+            this.groupBox1.Controls.Add(this.btnThree);
+            this.groupBox1.Controls.Add(this.btnTwo);
+            this.groupBox1.Controls.Add(this.btnOne);
+            this.groupBox1.Controls.Add(this.txtVisPadDisplay);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(207, 256);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "gboxMacroPadVis";
+            // 
+            // btnNine
+            // 
+            this.btnNine.Location = new System.Drawing.Point(139, 188);
+            this.btnNine.Name = "btnNine";
+            this.btnNine.Size = new System.Drawing.Size(60, 60);
+            this.btnNine.TabIndex = 9;
+            this.btnNine.Text = "Nine";
+            this.btnNine.UseVisualStyleBackColor = true;
+            this.btnNine.Click += new System.EventHandler(this.btnNine_Click);
+            // 
+            // btnEight
+            // 
+            this.btnEight.Location = new System.Drawing.Point(73, 188);
+            this.btnEight.Name = "btnEight";
+            this.btnEight.Size = new System.Drawing.Size(60, 60);
+            this.btnEight.TabIndex = 8;
+            this.btnEight.Text = "Eight";
+            this.btnEight.UseVisualStyleBackColor = true;
+            this.btnEight.Click += new System.EventHandler(this.btnEight_Click);
+            // 
+            // btnSeven
+            // 
+            this.btnSeven.Location = new System.Drawing.Point(6, 188);
+            this.btnSeven.Name = "btnSeven";
+            this.btnSeven.Size = new System.Drawing.Size(60, 60);
+            this.btnSeven.TabIndex = 7;
+            this.btnSeven.Text = "Seven";
+            this.btnSeven.UseVisualStyleBackColor = true;
+            this.btnSeven.Click += new System.EventHandler(this.btnSeven_Click);
+            // 
+            // btnSix
+            // 
+            this.btnSix.Location = new System.Drawing.Point(139, 122);
+            this.btnSix.Name = "btnSix";
+            this.btnSix.Size = new System.Drawing.Size(60, 60);
+            this.btnSix.TabIndex = 6;
+            this.btnSix.Text = "Six";
+            this.btnSix.UseVisualStyleBackColor = true;
+            this.btnSix.Click += new System.EventHandler(this.btnSix_Click);
+            // 
+            // btnFive
+            // 
+            this.btnFive.Location = new System.Drawing.Point(73, 122);
+            this.btnFive.Name = "btnFive";
+            this.btnFive.Size = new System.Drawing.Size(60, 60);
+            this.btnFive.TabIndex = 5;
+            this.btnFive.Text = "Five";
+            this.btnFive.UseVisualStyleBackColor = true;
+            this.btnFive.Click += new System.EventHandler(this.btnFive_Click);
+            // 
+            // btnFour
+            // 
+            this.btnFour.Location = new System.Drawing.Point(6, 122);
+            this.btnFour.Name = "btnFour";
+            this.btnFour.Size = new System.Drawing.Size(60, 60);
+            this.btnFour.TabIndex = 4;
+            this.btnFour.Text = "Four";
+            this.btnFour.UseVisualStyleBackColor = true;
+            this.btnFour.Click += new System.EventHandler(this.btnFour_Click);
+            // 
+            // btnThree
+            // 
+            this.btnThree.Location = new System.Drawing.Point(139, 56);
+            this.btnThree.Name = "btnThree";
+            this.btnThree.Size = new System.Drawing.Size(60, 60);
+            this.btnThree.TabIndex = 3;
+            this.btnThree.Text = "Three";
+            this.btnThree.UseVisualStyleBackColor = true;
+            this.btnThree.Click += new System.EventHandler(this.btnThree_Click);
+            // 
+            // btnTwo
+            // 
+            this.btnTwo.Location = new System.Drawing.Point(73, 56);
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.Size = new System.Drawing.Size(60, 60);
+            this.btnTwo.TabIndex = 2;
+            this.btnTwo.Text = "Two";
+            this.btnTwo.UseVisualStyleBackColor = true;
+            this.btnTwo.Click += new System.EventHandler(this.btnTwo_Click);
+            // 
+            // btnOne
+            // 
+            this.btnOne.Location = new System.Drawing.Point(7, 56);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(60, 60);
+            this.btnOne.TabIndex = 1;
+            this.btnOne.Text = "One";
+            this.btnOne.UseVisualStyleBackColor = true;
+            this.btnOne.Click += new System.EventHandler(this.btnOne_Click);
+            // 
+            // txtVisPadDisplay
+            // 
+            this.txtVisPadDisplay.Location = new System.Drawing.Point(6, 19);
+            this.txtVisPadDisplay.Name = "txtVisPadDisplay";
+            this.txtVisPadDisplay.ReadOnly = true;
+            this.txtVisPadDisplay.Size = new System.Drawing.Size(193, 20);
+            this.txtVisPadDisplay.TabIndex = 0;
+            // 
+            // txtButtonName
+            // 
+            this.txtButtonName.Location = new System.Drawing.Point(9, 106);
+            this.txtButtonName.Name = "txtButtonName";
+            this.txtButtonName.Size = new System.Drawing.Size(116, 20);
+            this.txtButtonName.TabIndex = 5;
+            // 
+            // lblNewButtonName
+            // 
+            this.lblNewButtonName.AutoSize = true;
+            this.lblNewButtonName.Location = new System.Drawing.Point(6, 90);
+            this.lblNewButtonName.Name = "lblNewButtonName";
+            this.lblNewButtonName.Size = new System.Drawing.Size(117, 13);
+            this.lblNewButtonName.TabIndex = 6;
+            this.lblNewButtonName.Text = "Enter new button name";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Play/Pause",
+            "Next Track",
+            "Previous Track",
+            "Volume Mute",
+            "Volume Up",
+            "Volume Down"});
+            this.listBox1.Location = new System.Drawing.Point(5, 171);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 43);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnNewLayer
+            // 
+            this.btnNewLayer.Location = new System.Drawing.Point(12, 274);
+            this.btnNewLayer.Name = "btnNewLayer";
+            this.btnNewLayer.Size = new System.Drawing.Size(207, 23);
+            this.btnNewLayer.TabIndex = 8;
+            this.btnNewLayer.Text = "Add New Layer";
+            this.btnNewLayer.UseVisualStyleBackColor = true;
+            this.btnNewLayer.Click += new System.EventHandler(this.btnNewLayer_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(137, 303);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(82, 23);
+            this.btnNext.TabIndex = 10;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(12, 303);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(82, 23);
+            this.btnPrev.TabIndex = 11;
+            this.btnPrev.Text = "Prev";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // txtSetLayerName
+            // 
+            this.txtSetLayerName.Location = new System.Drawing.Point(54, 358);
+            this.txtSetLayerName.Name = "txtSetLayerName";
+            this.txtSetLayerName.Size = new System.Drawing.Size(125, 20);
+            this.txtSetLayerName.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 342);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Enter Layer name";
+            // 
+            // btnSetLayername
+            // 
+            this.btnSetLayername.Location = new System.Drawing.Point(55, 385);
+            this.btnSetLayername.Name = "btnSetLayername";
+            this.btnSetLayername.Size = new System.Drawing.Size(124, 23);
+            this.btnSetLayername.TabIndex = 14;
+            this.btnSetLayername.Text = "Set layer name";
+            this.btnSetLayername.UseVisualStyleBackColor = true;
+            this.btnSetLayername.Click += new System.EventHandler(this.btnSetLayername_Click);
+            // 
+            // lblSelectedButton
+            // 
+            this.lblSelectedButton.AutoSize = true;
+            this.lblSelectedButton.Location = new System.Drawing.Point(6, 16);
+            this.lblSelectedButton.Name = "lblSelectedButton";
+            this.lblSelectedButton.Size = new System.Drawing.Size(89, 13);
+            this.lblSelectedButton.TabIndex = 15;
+            this.lblSelectedButton.Text = "Selected Button: ";
+            // 
+            // lblSelectedButtonName
+            // 
+            this.lblSelectedButtonName.AutoSize = true;
+            this.lblSelectedButtonName.Location = new System.Drawing.Point(6, 63);
+            this.lblSelectedButtonName.Name = "lblSelectedButtonName";
+            this.lblSelectedButtonName.Size = new System.Drawing.Size(72, 13);
+            this.lblSelectedButtonName.TabIndex = 16;
+            this.lblSelectedButtonName.Text = "Button Name:";
+            // 
+            // lblSelectedButtonOperation
+            // 
+            this.lblSelectedButtonOperation.AutoSize = true;
+            this.lblSelectedButtonOperation.Location = new System.Drawing.Point(6, 40);
+            this.lblSelectedButtonOperation.Name = "lblSelectedButtonOperation";
+            this.lblSelectedButtonOperation.Size = new System.Drawing.Size(90, 13);
+            this.lblSelectedButtonOperation.TabIndex = 17;
+            this.lblSelectedButtonOperation.Text = "Button Operation:";
+            // 
+            // lblNewButtonCommand
+            // 
+            this.lblNewButtonCommand.AutoSize = true;
+            this.lblNewButtonCommand.Location = new System.Drawing.Point(6, 129);
+            this.lblNewButtonCommand.Name = "lblNewButtonCommand";
+            this.lblNewButtonCommand.Size = new System.Drawing.Size(137, 13);
+            this.lblNewButtonCommand.TabIndex = 18;
+            this.lblNewButtonCommand.Text = "Enter new button command";
+            // 
+            // txtNewButtonCommand
+            // 
+            this.txtNewButtonCommand.Location = new System.Drawing.Point(6, 145);
+            this.txtNewButtonCommand.Name = "txtNewButtonCommand";
+            this.txtNewButtonCommand.Size = new System.Drawing.Size(119, 20);
+            this.txtNewButtonCommand.TabIndex = 19;
+            // 
+            // btnSetButton
+            // 
+            this.btnSetButton.Location = new System.Drawing.Point(7, 274);
+            this.btnSetButton.Name = "btnSetButton";
+            this.btnSetButton.Size = new System.Drawing.Size(116, 23);
+            this.btnSetButton.TabIndex = 20;
+            this.btnSetButton.Text = "Set Button";
+            this.btnSetButton.UseVisualStyleBackColor = true;
+            this.btnSetButton.Click += new System.EventHandler(this.btnSetButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.lblSelectedButton);
+            this.groupBox2.Controls.Add(this.btnSetButton);
+            this.groupBox2.Controls.Add(this.lblSelectedButtonName);
+            this.groupBox2.Controls.Add(this.txtNewButtonCommand);
+            this.groupBox2.Controls.Add(this.lblSelectedButtonOperation);
+            this.groupBox2.Controls.Add(this.lblNewButtonCommand);
+            this.groupBox2.Controls.Add(this.lblNewButtonName);
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Controls.Add(this.txtButtonName);
+            this.groupBox2.Location = new System.Drawing.Point(225, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(151, 314);
+            this.groupBox2.TabIndex = 21;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Button Control";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Clear Selection";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnSetLayername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSetLayerName);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnNewLayer);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnNine;
+        private System.Windows.Forms.Button btnEight;
+        private System.Windows.Forms.Button btnSeven;
+        private System.Windows.Forms.Button btnSix;
+        private System.Windows.Forms.Button btnFive;
+        private System.Windows.Forms.Button btnFour;
+        private System.Windows.Forms.Button btnThree;
+        private System.Windows.Forms.Button btnTwo;
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.TextBox txtVisPadDisplay;
+        private System.Windows.Forms.TextBox txtButtonName;
+        private System.Windows.Forms.Label lblNewButtonName;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnNewLayer;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.TextBox txtSetLayerName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSetLayername;
+        private System.Windows.Forms.Label lblSelectedButton;
+        private System.Windows.Forms.Label lblSelectedButtonName;
+        private System.Windows.Forms.Label lblSelectedButtonOperation;
+        private System.Windows.Forms.Label lblNewButtonCommand;
+        private System.Windows.Forms.TextBox txtNewButtonCommand;
+        private System.Windows.Forms.Button btnSetButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 3. Customisation/Macropad Customisation GUI Forms/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Macropad_Customisation_GUI_Forms
+{
+    public partial class Form1 : Form
+    {
+        List<MacroLayer> macroPad = new List<MacroLayer>();
+        string existingMacroPadLayout = "";
+        string macropadFileLocation = "";
+        int currentLayer = 0;
+        int currentButton = 0;
+        IDictionary<int, string> MacroKey = new Dictionary<int, string>();
+        public Form1()
+        {
+            InitializeComponent();
+            InitialiseKey();
+            MacroLayer ml = new MacroLayer();
+            macroPad.Add(ml);
+            //connectedMacroPad();
+        }
+        private void InitialiseKey()
+        {
+            MacroKey.Add(new KeyValuePair<int, string>(0, "One"));
+            MacroKey.Add(new KeyValuePair<int, string>(1, "Two"));
+            MacroKey.Add(new KeyValuePair<int, string>(2, "Three"));
+            MacroKey.Add(new KeyValuePair<int, string>(3, "Four"));
+            MacroKey.Add(new KeyValuePair<int, string>(4, "Five"));
+            MacroKey.Add(new KeyValuePair<int, string>(5, "Six"));
+            MacroKey.Add(new KeyValuePair<int, string>(6, "Seven"));
+            MacroKey.Add(new KeyValuePair<int, string>(7, "Eight"));
+            MacroKey.Add(new KeyValuePair<int, string>(8, "Nine"));
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void connectedMacroPad() //Will create or get the location of a config file
+        {
+            //Read from file
+            string message = "Is there a macropad connected?";
+            const string caption = "Select message macropad";
+            var result = MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes) { GetMacroLocation(); }
+            else { macropadFileLocation = ""; }
+
+        }
+        private void GetMacroLocation()
+        {
+            MessageBox.Show("Please select the file inside the connected macropad called: \nConfig.txt");
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    macropadFileLocation = openFileDialog.FileName;
+                }
+            }
+        }
+
+        //int 0 = prev int 1 = next
+        private void UpdateMacroPadDisplay(int boolPrevNext)
+        {
+            if (boolPrevNext == 0)
+            {
+                if (currentLayer == 0) { } //Top layer
+                else if (currentLayer != 0)
+                {
+                    currentLayer = currentLayer - 1;
+                }
+            }
+            if (boolPrevNext == 1)
+            {
+                if (currentLayer == macroPad.Count() - 1) { } //Bottom Layer
+                else { currentLayer = currentLayer + 1; }
+            }
+            txtVisPadDisplay.Text = macroPad[currentLayer].GetLayerName();
+        }
+        private void UpdateLayerName()
+        {
+            macroPad[currentLayer].SetLayerName(txtSetLayerName.Text);
+            txtVisPadDisplay.Text = macroPad[currentLayer].GetLayerName();
+            txtSetLayerName.Text = "";
+        }
+
+        private void UpdateButtonLabel(int index) 
+        {
+            lblSelectedButton.Text = "Selected Button: " + MacroKey[index];
+            currentButton = index;
+            lblSelectedButtonName.Text = "Button Name: " + macroPad[currentLayer].GetButtonName(index);
+            lblSelectedButtonOperation.Text = "Button Operation: " + macroPad[currentLayer].GetButtonCommand(index);
+        }
+
+        private void UpdateButton()
+        {
+            macroPad[currentLayer].SetButtonName(currentButton, txtButtonName.Text);
+            macroPad[currentLayer].SetButtonCommand(currentButton, txtNewButtonCommand.Text);
+        }
+
+        private void btnNewLayer_Click(object sender, EventArgs e)
+        {
+            MacroLayer ml = new MacroLayer();
+            macroPad.Add(ml);
+            UpdateMacroPadDisplay(1);
+        }
+        private void btnSetLayername_Click(object sender, EventArgs e) { UpdateLayerName(); }
+        private void btnPrev_Click(object sender, EventArgs e) { UpdateMacroPadDisplay(0); }
+        private void btnNext_Click(object sender, EventArgs e) { UpdateMacroPadDisplay(1); }
+
+
+
+        private void btnOne_Click(object sender, EventArgs e) { UpdateButtonLabel(0); }
+        private void btnTwo_Click(object sender, EventArgs e) { UpdateButtonLabel(1); }
+        private void btnThree_Click(object sender, EventArgs e) { UpdateButtonLabel(2); }
+        private void btnFour_Click(object sender, EventArgs e) { UpdateButtonLabel(3); }
+        private void btnFive_Click(object sender, EventArgs e) { UpdateButtonLabel( 4); }
+        private void btnSix_Click(object sender, EventArgs e) { UpdateButtonLabel(5); }
+        private void btnSeven_Click(object sender, EventArgs e) { UpdateButtonLabel(6); }
+        private void btnEight_Click(object sender, EventArgs e) { UpdateButtonLabel(7); }
+        private void btnNine_Click(object sender, EventArgs e) { UpdateButtonLabel( 8); }
+
+        private void btnSetButton_Click(object sender, EventArgs e){ UpdateButton(); }
+    }
+
+    public class MacroLayer
+    {
+        private string layerName = "";
+        private List<Button> buttonCommands = new List<Button>();
+
+        public MacroLayer()
+        {
+            layerName = "";
+            for (int i = 0; i < 9; i++)
+            {
+                Button b = new Button();
+                buttonCommands.Add(b);
+            }
+        }
+        public MacroLayer(int id, string name, List<Button> commands)
+        {
+            layerName = name;
+            buttonCommands = commands;
+        }
+        public void SetLayerName(string newName) { layerName = newName; }
+        public string GetLayerName() { return layerName; }
+
+        public void SetButtonName(int button, string name) { buttonCommands[button].SetButtonName(name); }
+        public void SetButtonCommand(int button, string command) { buttonCommands[button].SetButtonCommand(command); }
+
+        public string GetButtonName(int button) { return buttonCommands[button].GetButtonName(); }
+        public string GetButtonCommand(int button) { return buttonCommands[button].GetButtonOparation(); }
+    }
+
+    public class Button
+    {
+        private string buttonName;
+        private string buttonOperation;
+        public Button() 
+        {
+            buttonName = "";
+            buttonOperation = "";
+        }
+
+        public void SetButtonName(string name) { buttonName = name; }
+        public void SetButtonCommand(string command) { buttonOperation = command; }
+
+        public string GetButtonName() { return buttonName; }
+        public string GetButtonOparation() { return buttonOperation; }
+
+
+    }
+}
